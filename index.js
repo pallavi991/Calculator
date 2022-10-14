9c142a79c56cc34f6667f35c199b71093a2f83c2
Pallavi Sable <sablepallavi89@gmail.com>
2022-10-14 19:30:54 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("Hello world")
+
+const inp     =    document.getElementsByClassName("input");
+const AC      =    document.getElementById("AC");
+const one     =    document.getElementById("one");
+const two     =    document.getElementById("two");
+const three   =    document.getElementById("three");
+const four    =    document.getElementById("four");
+const five    =    document.getElementById("five");
+const six     =    document.getElementById("six");
+const seven   =    document.getElementById("seven");
+const eight   =    document.getElementById("eight");
+const nine    =    document.getElementById("nine");
+const zero    =    document.getElementById("zero");
+const divide  =    document.getElementById("divide");
+const star    =    document.getElementById("star");
+const equal   =    document.getElementById("equal");
+const sub     =    document.getElementById("sub");
+const period  =    document.getElementById("period");
+const plus    =    document.getElementById("plus");
+
+
+const btn = getElementsByClassName('btn');
+function myFunction() {
+    document.getElementsByClassName("intro").innerHTML = "BMW";
+  }
+ document.getElementsByClassName('btn'),addEventListener('click',myFunction)
